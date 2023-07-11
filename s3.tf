d4240a42bc2b0814e46e81f0823333336580bb19
brenonahum <137928984+brenonahum@users.noreply.github.com>
2023-07-11 10:21:31 -0300

    Add files via upload

---
+++ s3.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "default" {
+  bucket = join("-", [var.bucket_name, "youtube", var.env])
+  acl    = "private"
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.env
+    Version     = "v1.3.0"
+    Team        = var.team
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "bucket_name" {
+  description = "Name of the bucket"
+  type        = string
+}
+
+variable "env" {
+  description = "Environment where this module is invoked."
+  type        = string
+}
+
+variable "team" {
+  description = "Team responsible for the bucket."
+  type        = string
+}
+
+variable "aws_region" {
+  description = "aws region"
+  type        = string
+  default     = "us-east-1"
+
+}
